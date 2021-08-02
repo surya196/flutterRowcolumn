b83d9f42948a31ab6a8f3f6cf8a32c611a9fd7b2
surya <surya.m19698@gmail.com>
2021-08-02 10:14:55 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,124 @@
-import'package:flutter/material.dart';
-void main(){
+import 'package:flutter/material.dart';
+
+void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Row Column'),
+      ),
+      body: //Rowcolumn(),//Roww()
+          Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 50,
+              width: 50,
+              color: Colors.red,
+            ),
+            Container(
+              height: 50,
+              width: 50,
+              color: Colors.green,
+            ),
+            Container(
+              height: 50,
+              width: 50,
+              color: Colors.blue,
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
+
+class Roww extends StatelessWidget {
+  const Roww({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            height: 50,
+            width: 50,
+            color: Colors.red,
+          ),
+          Container(
+            height: 50,
+            width: 50,
+            color: Colors.green,
+          ),
+          Container(
+            height: 50,
+            width: 50,
+            color: Colors.blue,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Rowcolumn extends StatelessWidget {
+  const Rowcolumn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 50,
+            width: 50,
+            color: Colors.red,
+          ),
+          Container(
+            height: 50,
+            width: 50,
+            color: Colors.green,
+          ),
+          Container(
+            height: 50,
+            width: 50,
+            color: Colors.blue,
+          ),
+          Row(
+            children: [
+              Text('Row1'),
+              Text('Row2'),
+              Text('Row3'),
+              Text('Row4'),
+              Text('Row5'),
+              Text('Row6'),
+              Text('Row7'),
+              Text('Row8'),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
